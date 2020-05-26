8c29a9d24ea22c46a1dc6ffa0ea7507fa73abae6
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-05-26 22:19:42 +0530

    implement the rollbar

--- examples/index.js
+++ examples/index.js
@@ -2,16 +2,16 @@ const requestClient = require('../index');
 requestClient.defaultConfig({
 	baseURL: 'http://3.22.163.113/apis/v1/',
 	headers: {},
+	errorExpand: true,
 });
-const axio = requestClient.createInstance({
+const axios = requestClient.createInstance({
 	baseURL: 'http://pomurski-taborniki.eu/nature/apis/',
 	headers: {},
-	rollbarToken: '',
+	rollbarToken: 'bfca8b19cee44b6cb854bcb4cab26e97',
 	rollbarConfig: {
-		status: [400, 500],
+		status: [403, 500],
 	},
 });
-console.log(axio);
 const first = (config, next) => {
 	console.log(1);
 	next();
@@ -21,26 +21,23 @@ const second = (config, next) => {
 	next();
 };
 
-axio.use([first, second]);
+axios.use([first, second]);
 
 requestClient.middleware.request.use(
 	function (config) {
-		console.log(config);
 		config.headers['authorization_key'] =
 			'c5ae472377553a6923cadbbdf5e642bdfb6ee245';
-		console.log(config);
 		return config;
 	},
 	function (error) {
 		Promise.reject(error);
 	}
 );
-console.log(axio.getConfig);
-// axio
-// 	.post('/forgot_password', { goal_ids: 4 })
-// 	.then(({ status }) => {
-// 		console.log(status);
-// 	})
-// 	.catch(({ response }) => {
-// 		//console.log(response);
-// 	});
+axios
+	.post('/forgot_password', { email: 'pankaj@gmail.com' })
+	.then(({ status }) => {
+		console.log(status);
+	})
+	.catch(({ response }) => {
+		console.log(response);
+	});
