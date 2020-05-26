2b9d2d36bcb809f4147361019891c924da702082
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-05-26 12:39:58 +0530

    add middlewares and example

--- examples/index.js
+++ examples/index.js
@@ -2,8 +2,20 @@ const requestClient = require('../index');
 requestClient.defaultConfig({
 	baseURL: 'http://3.12.247.224/apis/v1/',
 });
+const first = (config, next) => {
+	console.log(1, config);
+	next();
+};
+const second = (config, next) => {
+	console.log(2);
+	next();
+};
+
+requestClient.use([first, second]);
+
 requestClient.middleware.request.use(
 	function (config) {
+		console.log(config);
 		return config;
 	},
 	function (error) {
@@ -13,7 +25,7 @@ requestClient.middleware.request.use(
 requestClient
 	.get('/app-information')
 	.then((data) => {
-		console.log(data);
+		//console.log(data);
 	})
 	.catch((response) => {
 		console.log(response);
