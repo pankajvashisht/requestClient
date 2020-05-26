8bfcb16665931c29c302536edb1072024d8c8d7c
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-05-26 10:51:39 +0530

    add middleware hook

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const requestClient = require('./lib/requestClient');
 requestClient.defaultConfig({
 	baseURL: 'http://3.12.247.224/apis/v1/',
 });
-
+requestClient.middleware.request.use(
+	function (config) {
+		console.log('1');
+		return config;
+	},
+	function (error) {
+		Promise.reject(error);
+	}
+);
 requestClient
 	.get('/app-information')
-	.then(({ data, status }) => {
-		console.log(data.message);
+	.then((data) => {
+		console.log(data);
 	})
-	.catch(({ response }) => {
+	.catch((response) => {
 		console.log(response);
 	});
